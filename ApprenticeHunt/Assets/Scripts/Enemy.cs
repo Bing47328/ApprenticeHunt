9ba16bfe061cb42d47135daf8c4f38c16f2d7ff5
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-04-19 11:05:16 -0400

    Unity

---
+++ ApprenticeHunt/Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float maxHP = 100f;
+    public float currentHP;
+    public Image health;
+    public Image healthEffect;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    private void Update()
+    {
+        DiplayHealthBar();
+
+        if (currentHP <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    protected virtual void DiplayHealthBar()
+    {
+        health.fillAmount = currentHP / maxHP;
+
+        if (healthEffect.fillAmount > health.fillAmount)
+        {
+            healthEffect.fillAmount -= 0.005f;
+        }
+        else
+        {
+            healthEffect.fillAmount = health.fillAmount;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Projectile")
+            currentHP -= 15;
+
+    }
+}
---
+++ ApprenticeHunt/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
