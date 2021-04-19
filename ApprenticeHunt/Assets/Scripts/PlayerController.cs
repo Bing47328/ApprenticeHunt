5c37eec51beb6ae616dc7ea0e0aae195f383d7e2
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-04-18 20:38:28 -0400

    Unity

---
+++ ApprenticeHunt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector3 change;
+    bool faceR = false;
+
+    private Animator animator;
+    private string currentState;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            ChangingAnimationState("Walk");
+        }
+        else
+        {
+            ChangingAnimationState("Idle");
+        }
+
+        if (change.x < 0 && faceR)
+        {
+            flip();
+        }
+        else if (change.x > 0 && !faceR)
+        {
+            flip();
+        }
+
+    }
+
+    void ChangingAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+
+    void MoveCharacter()
+    {
+        rb.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+
+    void flip()
+    {
+        faceR = !faceR;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
