a929c26beb4805fe00f839f2b41f11cbd32ddfda
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-04-26 00:42:08 -0400

    Unity

--- ApprenticeHunt/Assets/Scripts/Enemy.cs
+++ ApprenticeHunt/Assets/Scripts/Enemy.cs
@@ -5,19 +5,35 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    // Health Bar
     public float maxHP = 100f;
     public float currentHP;
     public Image health;
     public Image healthEffect;
 
+    // Movement
+    public float moveSpeed;
+    private Transform target;
+    private SpriteRenderer sr;
+
+    public Transform wayPoint1, wayPoint2;
+    private Transform wayPointTarget;
+    private float distance;
+
+
     private void Awake()
     {
         currentHP = maxHP;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        wayPointTarget = wayPoint1;
     }
 
     private void Update()
     {
         DiplayHealthBar();
+        Move();
+        TurnDirection();
 
         if (currentHP <= 0)
         {
@@ -25,9 +41,40 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void Move()
+    {
+        if (Vector2.Distance(transform.position, target.position) > distance)
+        {
+
+            if (Vector2.Distance(transform.position, wayPoint1.position) < 0.01f)
+            {
+                wayPointTarget = wayPoint2;
+            }
+            if (Vector2.Distance(transform.position, wayPoint2.position) < 0.01f)
+            {
+                wayPointTarget = wayPoint1;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, wayPointTarget.position, moveSpeed * Time.deltaTime);
+
+        }
+           // transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+
     void Death()
     {
         Destroy(gameObject);
+    }
+
+    void TurnDirection()
+    {
+        if (transform.position.x > target.position.x)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
     }
 
     protected virtual void DiplayHealthBar()
