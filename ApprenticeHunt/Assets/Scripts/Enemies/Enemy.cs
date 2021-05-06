c88dc94b0d47061ab7dda65a7405dae4c0acaa9c
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-05-06 17:05:39 -0400

    Unity

---
+++ ApprenticeHunt/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    // Health Bar
+    public float maxHP = 100f;
+    public float currentHP;
+    public Image health;
+    public Image healthEffect;
+
+    // Movement
+    [SerializeField]protected private float moveSpeed = 1;
+    protected private Transform target;
+    private SpriteRenderer sr;
+    [SerializeField]protected private float distance = 4;
+
+
+    private void Start()
+    {
+        currentHP = maxHP;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        DiplayHealthBar();
+        TurnDirection();
+
+        if (currentHP <= 0)
+        {
+            Death();
+        }
+
+        Attack();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    protected virtual void Move()
+    {
+        if (Vector2.Distance(transform.position, target.position) < distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    protected virtual void TurnDirection()
+    {
+        if (transform.position.x > target.position.x)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+
+    protected virtual void DiplayHealthBar()
+    {
+        health.fillAmount = currentHP / maxHP;
+
+        if (healthEffect.fillAmount > health.fillAmount)
+        {
+            healthEffect.fillAmount -= 0.005f;
+        }
+        else
+        {
+            healthEffect.fillAmount = health.fillAmount;
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log(gameObject + "is Attacking");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Projectile")
+            currentHP -= 15;
+
+    }
+}
---
+++ ApprenticeHunt/Assets/Scripts/Scenes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    void MainMenu()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            MainMenu();
+        }
+    }
+}
